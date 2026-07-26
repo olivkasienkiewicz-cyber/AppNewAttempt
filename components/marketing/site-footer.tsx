646404dbc79d7904128eb2336732b4b7161fa87c
olivkasienkiewicz-cyber <olivkasienkiewicz@gmail.com>
2026-07-26 19:24:02 +0200

    Create site-footer.tsx

---
+++ components/marketing/site-footer.tsx
@@ -0,0 +1,7 @@
+export function SiteFooter() {
+  return (
+    <footer className="border-t border-[#0E2A47]/10 py-8 text-center text-xs text-[#0E2A47]/50">
+      © {new Date().getFullYear()} Studilly
+    </footer>
+  );
+}
