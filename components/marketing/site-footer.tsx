1a44c85e3884e0462ba6dfdb437ad8c8ad34b933
olivkasienkiewicz-cyber <olivkasienkiewicz@gmail.com>
2026-07-27 12:21:10 +0200

    Delete components/marketing directory

--- components/marketing/site-footer.tsx
+++
@@ -1,7 +0,0 @@
-export function SiteFooter() {
-  return (
-    <footer className="border-t border-[#0E2A47]/10 py-8 text-center text-xs text-[#0E2A47]/50">
-      © {new Date().getFullYear()} Studilly
-    </footer>
-  );
-}
