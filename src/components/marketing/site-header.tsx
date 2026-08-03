e75122a49eb677a419e5750c2723868660250414
olivkasienkiewicz-cyber <olivkasienkiewicz@gmail.com>
2026-08-03 09:47:06 +0200

    Update site-header.tsx

--- src/components/marketing/site-header.tsx
+++ src/components/marketing/site-header.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { LanguageToggle } from "./LanguageToggle";
+import { LanguageToggle } from "../LanguageToggle";
 
 export function SiteHeader() {
   return (
